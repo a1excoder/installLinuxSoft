d0d08dc111b270452e0a42988d6b995727f6ac60
Aleksandr Rudenko <49398367+a1excoder@users.noreply.github.com>
2021-07-25 23:35:16 +0300

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,23 @@
+sudo apt-get update && apt-get upgrade -y
+
+sudo apt install gcc -y && sudo apt install g++ -y && sudo apt install make -y
+sudo apt install micro -y
+sudo apt install php -y
+
+sudo apt install qbittorrent -y
+sudo apt install blender -y
+sudo apt install krita -y
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+sudo apt install telegram-desktop -y
+sudo apt install flameshot -y
+sudo apt install vlc -y
+sudo apt install audacity -y
+sudo apt install obs-studio -y
+sudo apt install gparted -y
+sudo apt install git -y
