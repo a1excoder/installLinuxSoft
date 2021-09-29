0b7f8c3e1e1eb1b62bf68bf4c7561df28e8e6e5e
Aleksandr Rudenko <49398367+a1excoder@users.noreply.github.com>
2021-09-29 11:07:46 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,18 +1,25 @@
+#!/bin/bash
+
 sudo apt-get update && apt-get upgrade -y
 
 sudo apt install gcc -y && sudo apt install g++ -y && sudo apt install make -y
 sudo apt install micro -y
 sudo apt install php -y
+sudo apt install python3-pip
 
 sudo apt install qbittorrent -y
-sudo apt install blender -y
 sudo apt install krita -y
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install sublime-text
+# sublime text
+sublime_install () {
+  wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+  sudo apt-get install apt-transport-https
+  echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+  sudo apt-get update
+  sudo apt-get install sublime-text
+}
+
+sublime_install
 
 sudo apt install telegram-desktop -y
 sudo apt install flameshot -y
