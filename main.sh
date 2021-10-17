779d8fd22e135db80a8c54a0e986323fc828129e
Aleksandr Rudenko <49398367+a1excoder@users.noreply.github.com>
2021-10-17 15:48:51 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ sudo apt-get update && apt-get upgrade -y
 sudo apt install gcc -y && sudo apt install g++ -y && sudo apt install make -y
 sudo apt install micro -y
 sudo apt install php -y
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 
 sudo apt install qbittorrent -y
 sudo apt install krita -y
@@ -13,10 +13,10 @@ sudo apt install krita -y
 # sublime text
 sublime_install () {
   wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-  sudo apt-get install apt-transport-https
+  sudo apt-get install apt-transport-https -y
   echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
   sudo apt-get update
-  sudo apt-get install sublime-text
+  sudo apt-get install sublime-text -y
 }
 
 sublime_install
